0456a7083bfdc2df0cfa7dff07079407dc94c26d
Bouxi <56498736+bouxi@users.noreply.github.com>
2025-01-28 12:52:51 +0100

    base

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+$title = "Page d'accueil";
+require 'template/header.php';
+?>
+
+
+<h1>Hello, world!</h1>
+
+
+<?php require 'template/footer.php'; ?>
---
+++ template/fonctions.php
@@ -0,0 +1,21 @@
+<?php
+function nav_item(string $lien, string $titre, string $linkClass = ''): string
+{
+    $classe = 'nav-link';
+    if ($_SERVER['SCRIPT_NAME'] === $lien) {
+        $classe .= ' active';
+    }
+    return <<<HTML
+        <li class="$linkClass">
+            <a class="$classe" href="$lien">$titre</a>
+        </li>
+HTML;
+}
+
+function nav_menu (string $linkClass = ''): string
+{
+    return
+        nav_item('/index.php', 'Accueil', $linkClass) .
+        nav_item('/contact.php', 'Contact', $linkClass);
+}
+?>
